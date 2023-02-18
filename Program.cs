30b3fcdbe90bb35376c0ac3f3e77e67863386176
Brenno Prettyman <brennoprettyman@gmail.com>
2023-02-18 12:52:37 -0300

    Adiciona letra de música

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Musica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Smells Like Teen Spirit - Nirvana");
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.WriteLine("Load up on guns, bring your friends");
+            Console.WriteLine("It's fun to lose and to pretend");
+            Console.WriteLine("She's over bored and self assured");
+            Console.WriteLine("Oh no, I know a dirty word");
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Hello, hello, hello, how low");
+            Console.WriteLine("Hello, hello, hello, how low");
+            Console.WriteLine("Hello, hello, hello, how low");
+            Console.WriteLine("Hello, hello, hello");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("With the lights out, it's less dangerous");
+            Console.WriteLine("Here we are now, entertain us");
+            Console.WriteLine("I feel stupid and contagious");
+            Console.WriteLine("Here we are now, entertain us");
+            Console.WriteLine("A mulatto, an albino, a mosquito, my libido");
+            Console.WriteLine("Yeah, hey");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Yay");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("I'm worse at what I do best");
+            Console.WriteLine("And for this gift I feel blessed");
+            Console.WriteLine("Our little group has always been");
+            Console.WriteLine("And always will until the end");
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Hello, hello, hello, how low");
+            Console.WriteLine("Hello, hello, hello, how low");
+            Console.WriteLine("Hello, hello, hello, how low");
+            Console.WriteLine("Hello, hello, hello");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("With the lights out, it's less dangerous");
+            Console.WriteLine("Here we are now, entertain us");
+            Console.WriteLine("I feel stupid and contagious");
+            Console.WriteLine("Here we are now, entertain us");
+            Console.WriteLine("A mulatto, an albino, a mosquito, my libido");
+            Console.WriteLine("Yeah, hey");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Yay");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("And I forget just why I taste");
+            Console.WriteLine("Oh yeah, I guess it makes me smile");
+            Console.WriteLine("I found it hard, was hard to find");
+            Console.WriteLine("Oh well, whatever, never mind");
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Hello, hello, hello, how low");
+            Console.WriteLine("Hello, hello, hello, how low");
+            Console.WriteLine("Hello, hello, hello, how low");
+            Console.WriteLine("Hello, hello, hello");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("With the lights out, it's less dangerous");
+            Console.WriteLine("Here we are now, entertain us");
+            Console.WriteLine("I feel stupid and contagious");
+            Console.WriteLine("Here we are now, entertain us");
+            Console.WriteLine("A mulatto, an albino, a mosquito, my libido");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("A denial, a denial, a denial, a denial, a denial");
+            Console.WriteLine("A denial, a denial, a denial, a denial");
+            Console.WriteLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Musica
+
+Exercício Etec:
 Faça um programa que exiba uma letra de música a sua escolha. Utilize cores em destaque no título e nos refrões.
